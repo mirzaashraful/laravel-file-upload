cb596e08c27a2e341b4478250a4bf90e0bf76534
mirzaashraful <mirza.ashraful@gmail.com>
2024-09-24 13:26:59 +0600

    controller create

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/profile/{id}',[ProfileController::class,'index']);
-//Route::get('/profile',[ProfileController::class,'index']);
 
